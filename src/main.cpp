82dd7378b9a125561917262bdca0d8de78a7277b
Miguel Ferrer <mferrer@inbiot.es>
2024-12-16 12:57:18 +0100

    start commit

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include "Ymodem/ymodem.h"
+#include <Arduino.h>
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  Serial.println("Hello World");
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
