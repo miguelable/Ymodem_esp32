a8098acfb18f2be34e26234d3281bbedc1221af9
Miguel Ferrer <mferrer@inbiot.es>
2025-01-22 11:25:39 +0100

    Refactor Ymodem class by removing unused methods and adding packet preparation functions; update VSCode settings for file associations and enable monitor filters in platformio.ini

--- lib/Ymodem/ymodem.cpp
+++ lib/Ymodem/ymodem.cpp
@@ -32,11 +32,6 @@
 #include "rom/crc.h"
 #include <driver/uart.h>
 
-Ymodem::Ymodem()
-{
-  pinMode(ledPin, OUTPUT);
-}
-
 //----------------------------------
 static void IRAM_ATTR LED_toggle()
 {
@@ -48,19 +43,6 @@ static void IRAM_ATTR LED_toggle()
     GPIO.out_w1ts = (1 << YMODEM_LED_ACT);
   }
 #endif
-}
-
-void Ymodem::setLedPin(int pin)
-{
-  ledPin = pin;
-  // Set the LED pin as an output
-  pinMode(ledPin, OUTPUT);
-  log_i("LED pin set to %d", ledPin);
-}
-
-int Ymodem::getLedPin()
-{
-  return ledPin;
 }
 
 //------------------------------------------------------------------------
@@ -141,19 +123,6 @@ static void send_CRC16()
   Send_Byte(CRC16);
 }
 
-/**
- * @brief  Receive a packet from sender
- * @param  data
- * @param  timeout
- * @param  length
- *    >0: packet length
- *     0: end of transmission
- *    -1: abort by sender
- *    -2: error or crc error
- * @retval 0: normally return
- *        -1: timeout
- *        -2: abort by user
- */
 //--------------------------------------------------------------------------
 static int32_t Receive_Packet(uint8_t* data, int* length, uint32_t timeout)
 {
@@ -220,8 +189,120 @@ static int32_t Receive_Packet(uint8_t* data, int* length, uint32_t timeout)
   return 0;
 }
 
-// Receive a file using the ymodem protocol.
-//-----------------------------------------------------------------
+//------------------------------------------------------------------------------------
+static void Ymodem_PrepareIntialPacket(uint8_t* data, char* fileName, uint32_t length)
+{
+  uint16_t tempCRC;
+
+  memset(data, 0, PACKET_SIZE + PACKET_HEADER);
+  // Make first three packet
+  data[0] = SOH;
+  data[1] = 0x00;
+  data[2] = 0xff;
+
+  // add filename
+  sprintf((char*)(data + PACKET_HEADER), "%s", fileName);
+
+  // add file site
+  sprintf((char*)(data + PACKET_HEADER + strlen((char*)(data + PACKET_HEADER)) + 1), "%d", length);
+  data[PACKET_HEADER + strlen((char*)(data + PACKET_HEADER)) + 1 +
+       strlen((char*)(data + PACKET_HEADER + strlen((char*)(data + PACKET_HEADER)) + 1))] = ' ';
+
+  // add crc
+  tempCRC                               = crc16(&data[PACKET_HEADER], PACKET_SIZE);
+  data[PACKET_SIZE + PACKET_HEADER]     = tempCRC >> 8;
+  data[PACKET_SIZE + PACKET_HEADER + 1] = tempCRC & 0xFF;
+}
+
+//-------------------------------------------------
+static void Ymodem_PrepareLastPacket(uint8_t* data)
+{
+  uint16_t tempCRC;
+
+  memset(data, 0, PACKET_SIZE + PACKET_HEADER);
+  data[0] = SOH;
+  data[1] = 0x00;
+  data[2] = 0xff;
+  tempCRC = crc16(&data[PACKET_HEADER], PACKET_SIZE);
+  // tempCRC = crc16_le(0, &data[PACKET_HEADER], PACKET_SIZE);
+  data[PACKET_SIZE + PACKET_HEADER]     = tempCRC >> 8;
+  data[PACKET_SIZE + PACKET_HEADER + 1] = tempCRC & 0xFF;
+}
+
+//-----------------------------------------------------------------------------------------
+static void Ymodem_PreparePacket(uint8_t* data, uint8_t pktNo, uint32_t sizeBlk, fs::File& ffd)
+{
+  uint16_t i, size;
+  uint16_t tempCRC;
+
+  data[0] = STX;
+  data[1] = (pktNo & 0x000000ff);
+  data[2] = (~(pktNo & 0x000000ff));
+
+  size = sizeBlk < PACKET_1K_SIZE ? sizeBlk : PACKET_1K_SIZE;
+  // Read block from file
+  if (size > 0) {
+    size = ffd.read(data + PACKET_HEADER, size);
+  }
+
+  if (size < PACKET_1K_SIZE) {
+    for (i = size + PACKET_HEADER; i < PACKET_1K_SIZE + PACKET_HEADER; i++) {
+      data[i] = 0x00; // EOF (0x1A) or 0x00
+    }
+  }
+  tempCRC = crc16(&data[PACKET_HEADER], PACKET_1K_SIZE);
+  // tempCRC = crc16_le(0, &data[PACKET_HEADER], PACKET_1K_SIZE);
+  data[PACKET_1K_SIZE + PACKET_HEADER]     = tempCRC >> 8;
+  data[PACKET_1K_SIZE + PACKET_HEADER + 1] = tempCRC & 0xFF;
+}
+
+//-------------------------------------------------------------
+static uint8_t Ymodem_WaitResponse(uint8_t ackchr, uint8_t tmo)
+{
+  unsigned char receivedC;
+  uint32_t      errors = 0;
+
+  do {
+    if (Receive_Byte(&receivedC, NAK_TIMEOUT) == 0) {
+      if (receivedC == ackchr) {
+        return 1;
+      }
+      else if (receivedC == CA) {
+        send_CA();
+        return 2; // CA received, Sender abort
+      }
+      else if (receivedC == NAK) {
+        return 3;
+      }
+      else {
+        return 4;
+      }
+    }
+    else {
+      errors++;
+    }
+  } while (errors < tmo);
+  return 0;
+}
+
+Ymodem::Ymodem()
+{
+  pinMode(ledPin, OUTPUT);
+}
+
+void Ymodem::setLedPin(int pin)
+{
+  ledPin = pin;
+  // Set the LED pin as an output
+  pinMode(ledPin, OUTPUT);
+  log_i("LED pin set to %d", ledPin);
+}
+
+int Ymodem::getLedPin()
+{
+  return ledPin;
+}
+
 /**
  * @brief Receives a file using the Ymodem protocol.
  *
@@ -408,102 +489,6 @@ exit:
   digitalWrite(ledPin, YMODEM_LED_ACT_ON ^ 1);
 #endif
   return size;
-}
-
-//------------------------------------------------------------------------------------
-static void Ymodem_PrepareIntialPacket(uint8_t* data, char* fileName, uint32_t length)
-{
-  uint16_t tempCRC;
-
-  memset(data, 0, PACKET_SIZE + PACKET_HEADER);
-  // Make first three packet
-  data[0] = SOH;
-  data[1] = 0x00;
-  data[2] = 0xff;
-
-  // add filename
-  sprintf((char*)(data + PACKET_HEADER), "%s", fileName);
-
-  // add file site
-  sprintf((char*)(data + PACKET_HEADER + strlen((char*)(data + PACKET_HEADER)) + 1), "%d", length);
-  data[PACKET_HEADER + strlen((char*)(data + PACKET_HEADER)) + 1 +
-       strlen((char*)(data + PACKET_HEADER + strlen((char*)(data + PACKET_HEADER)) + 1))] = ' ';
-
-  // add crc
-  tempCRC                               = crc16(&data[PACKET_HEADER], PACKET_SIZE);
-  data[PACKET_SIZE + PACKET_HEADER]     = tempCRC >> 8;
-  data[PACKET_SIZE + PACKET_HEADER + 1] = tempCRC & 0xFF;
-}
-
-//-------------------------------------------------
-static void Ymodem_PrepareLastPacket(uint8_t* data)
-{
-  uint16_t tempCRC;
-
-  memset(data, 0, PACKET_SIZE + PACKET_HEADER);
-  data[0] = SOH;
-  data[1] = 0x00;
-  data[2] = 0xff;
-  tempCRC = crc16(&data[PACKET_HEADER], PACKET_SIZE);
-  // tempCRC = crc16_le(0, &data[PACKET_HEADER], PACKET_SIZE);
-  data[PACKET_SIZE + PACKET_HEADER]     = tempCRC >> 8;
-  data[PACKET_SIZE + PACKET_HEADER + 1] = tempCRC & 0xFF;
-}
-
-//-----------------------------------------------------------------------------------------
-static void Ymodem_PreparePacket(uint8_t* data, uint8_t pktNo, uint32_t sizeBlk, fs::File& ffd)
-{
-  uint16_t i, size;
-  uint16_t tempCRC;
-
-  data[0] = STX;
-  data[1] = (pktNo & 0x000000ff);
-  data[2] = (~(pktNo & 0x000000ff));
-
-  size = sizeBlk < PACKET_1K_SIZE ? sizeBlk : PACKET_1K_SIZE;
-  // Read block from file
-  if (size > 0) {
-    size = ffd.read(data + PACKET_HEADER, size);
-  }
-
-  if (size < PACKET_1K_SIZE) {
-    for (i = size + PACKET_HEADER; i < PACKET_1K_SIZE + PACKET_HEADER; i++) {
-      data[i] = 0x00; // EOF (0x1A) or 0x00
-    }
-  }
-  tempCRC = crc16(&data[PACKET_HEADER], PACKET_1K_SIZE);
-  // tempCRC = crc16_le(0, &data[PACKET_HEADER], PACKET_1K_SIZE);
-  data[PACKET_1K_SIZE + PACKET_HEADER]     = tempCRC >> 8;
-  data[PACKET_1K_SIZE + PACKET_HEADER + 1] = tempCRC & 0xFF;
-}
-
-//-------------------------------------------------------------
-static uint8_t Ymodem_WaitResponse(uint8_t ackchr, uint8_t tmo)
-{
-  unsigned char receivedC;
-  uint32_t      errors = 0;
-
-  do {
-    if (Receive_Byte(&receivedC, NAK_TIMEOUT) == 0) {
-      if (receivedC == ackchr) {
-        return 1;
-      }
-      else if (receivedC == CA) {
-        send_CA();
-        return 2; // CA received, Sender abort
-      }
-      else if (receivedC == NAK) {
-        return 3;
-      }
-      else {
-        return 4;
-      }
-    }
-    else {
-      errors++;
-    }
-  } while (errors < tmo);
-  return 0;
 }
 
 //------------------------------------------------------------------------
